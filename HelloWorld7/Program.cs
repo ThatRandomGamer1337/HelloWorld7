d55bef6b6b8f65aedff41f99373aad8ac6b527e6
ThatRandomGamer1337 <113189409+ThatRandomGamer1337@users.noreply.github.com>
2022-09-13 09:12:24 -0300

    Initial commit

---
+++ HelloWorld7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World. THIS time I've got this working correctly lol");
+            Console.ReadKey(true);
+        }
+    }
+}
